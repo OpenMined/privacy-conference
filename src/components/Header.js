596c0fcc2a296a16b1468958be1d2da94f30931f
Patrick Cason <me@patrickcason.com>
2020-08-21 11:05:19 +0100

    Trying to fix mobile gradient

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ export default ({ headerHeight, sidebarWidth, footerHeight }) => {
           bottom: -10,
           width: '100%',
           height: 10,
-          background: `linear-gradient(0deg, rgba(0,0,0,0) 0%, ${theme.colors.muted} 100%)`
+          background: `linear-gradient(0deg, rgba(255,255,255,0) 0%, ${theme.colors.muted} 100%)`
         }
       }}
     >
